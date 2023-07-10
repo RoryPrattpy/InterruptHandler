40822e396820d8b226d3325a63bbc8a64095965b
RoryPrattpy <rryprtt@gmail.com>
2023-07-09 19:51:50 -0500

    ff

--- src/InterruptHandler.ino
+++ src/InterruptHandler.ino
@@ -1,18 +1,70 @@
-/*
- * Project InterruptHandler
- * Description:
- * Author:
- * Date:
- */
+#define incrementBtn D8
+#define stopBtn D7
 
-// setup() runs once, when the device is first turned on.
+#include "oled-wing-adafruit.h"
+
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+OledWingAdafruit display;
+
+volatile bool interrupt = false;
+bool interuptsAllowed = true;
+int counter = 0;
+bool readButtonValue = true;
+
 void setup() {
-  // Put initialization like pinMode and begin functions here.
+  pinMode(incrementBtn, INPUT_PULLUP);
+  pinMode(stopBtn, INPUT_PULLUP);
 
+  attachInterrupt(incrementBtn, isr, FALLING);
+
+  display.setup();
+
+  resetDisplay();
+	display.display();
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  // The core of your code will likely live here.
+  if (interrupt) {
+    counter++;
 
+    interrupt = false;
+  }
+
+  if (!digitalRead(stopBtn) && readButtonValue) {
+    if (interuptsAllowed) {
+      noInterrupts();
+      interuptsAllowed = false;
+    } else {
+      interrupts();
+      interuptsAllowed = true;
+    }
+    readButtonValue = false;
+    interrupt = false;
+  }
+
+  if (digitalRead(stopBtn)) {
+    readButtonValue = true;
+  }
+
+  resetDisplay();
+  updateDisplay();
+}
+
+void isr() {
+  interrupt = true;
+}
+
+void resetDisplay() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+}
+
+void updateDisplay() {
+  display.print("Counter: ");
+  display.print(counter);
+  display.display();
 }
